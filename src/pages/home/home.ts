55321445ea91f373cc21a69b5e02d556a43ed131
Tony Nguyen <tnguyen15@live.maryville.edu>
2021-11-14 18:25:33 -0800

    Add files via upload

---
+++ src/pages/home/home.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular'
+import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery"
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController,
+    public alertCtrl: AlertController, public dataService: GroceriesServiceProvider,
+    public inputDialogService: InputDialogServiceProvider) {
+
+  }
+
+  loadData() {
+    return this.dataService.getItems()
+  }
+
+  removeItem(item, index) {
+    console.log("removing item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Removing item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+    this.dataService.removeItem(index)
+  }
+
+  editItem(item, index) {
+    console.log("edit item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Editing item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+    this.inputDialogService.showPrompt(item, index);
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.inputDialogService.showPrompt();
+  }
+}
